88bac42985b76ed83587491519df92b36d0a85d7
Mustofa <mustofa_sachi@hotmail.com>
2020-11-05 01:29:40 +0700

    Add Questions

--- src/components/quiz/Play.js
+++ src/components/quiz/Play.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Helmet } from 'react-helmet'
+import ques from '../../questions.json'
 
 const Play = () => {
+    const [questions, setQuestions] = useState(ques);
+    const [currentQuestion, setCurrentQuestion] = useState({});
+    const [nextQuestion, setNextQuestion] = useState({});
+    const [previousQuestion, setPreviousQuestion] = useState({});
+    const [numberOfQuestion, setNumberOfQuestion] = useState(0);
+    const [numberOfAnsweredQuestion, setNumberOfAnsweredQuestion] = useState(0);
+    const [currentQuestionIndex, setcurrentQuestionIndex] = useState(0);
+    const [score, setScore] = useState(0);
+    const [correctAnswer, setCorrectAnswer] = useState(0);
+    const [wronngAnswers, setWrongAnswers] = useState(0);
+    const [hints, setHints] = useState(5);
+    const [fiftyFifty, setFiftyFifty] = useState(0);
+    const [usedFiftyFifty, setUsedFiftyFifty] = useState(false);
+    const [time, setTime] = useState({});
+
+    const displayQuestions = () => {
+        if (questions?.length) {
+            setCurrentQuestion(questions[currentQuestionIndex]);
+            setNextQuestion(questions[currentQuestionIndex + 1]);
+            setPreviousQuestion(questions[currentQuestionIndex - 1]);
+        }
+    }
+
+    useEffect(() => {
+        displayQuestions();
+    }, []);
+
+    // console.log("Answer is >> ", answer);
+    console.log("Current Question >> ", currentQuestion);
+    console.log("Previous Question >> ", previousQuestion);
+    console.log("Next Question >> ", nextQuestion);
+    console.log("Answer >> ", currentQuestion.answer);
+
     return (
         <>
             <Helmet><title>Quiz Page</title></Helmet>
@@ -27,16 +61,15 @@ const Play = () => {
                     </p>
                 </div>
                 <div className="question">
-                    <h5>Tony : __________________</h5>
-                    <h5>Clay : The third.</h5>
+                    <h5>{currentQuestion.question}</h5>
                 </div>
                 <div className="option-container">
-                    <p className="option">What time is it?</p>
-                    <p className="option">How much is the apple?</p>
+                    <p className="option">{currentQuestion.optionA}</p>
+                    <p className="option">{currentQuestion.optionB}</p>
                 </div>
                 <div className="option-container">
-                    <p className="option">How many rooms are there?</p>
-                    <p className="option">Which floor are you going to?</p>
+                    <p className="option">{currentQuestion.optionC}</p>
+                    <p className="option">{currentQuestion.optionD}</p>
                 </div>
                 <div className="button-container">
                     <button>Previous</button>
