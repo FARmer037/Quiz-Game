158968c1440c6df47bc33444bc29797b91dfdc89
Mustofa <mustofa_sachi@hotmail.com>
2020-11-11 11:48:14 +0700

    Handle hints

--- src/components/quiz/Play.js
+++ src/components/quiz/Play.js
@@ -5,19 +5,23 @@ import M from 'materialize-css'
 import correctNotification from '../../assets/audio/correct-answer.mp3'
 import wrongNotification from '../../assets/audio/wrong-answer.mp3'
 import buttonNotification from '../../assets/audio/button-sound.mp3'
+import { useHistory } from 'react-router-dom'
 
 const Play = () => {
+    const history = useHistory();
+
     const [currentQuestion, setCurrentQuestion] = useState({});
     const [nextQuestion, setNextQuestion] = useState({});
     const [previousQuestion, setPreviousQuestion] = useState({});
     const [numberOfQuestion, setNumberOfQuestion] = useState(0);
     const [numberOfAnsweredQuestion, setNumberOfAnsweredQuestion] = useState(0);
-    const [currentQuestionIndex, setcurrentQuestionIndex] = useState(0);
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [score, setScore] = useState(0);
     const [correctAnswer, setCorrectAnswer] = useState(0);
     const [wrongAnswers, setWrongAnswers] = useState(0);
     const [hints, setHints] = useState(5);
-    const [fiftyFifty, setFiftyFifty] = useState(0);
+    const [previousRandomNumbers, setPreviousRandomNumbers] = useState([]);
+    const [fiftyFifty, setFiftyFifty] = useState(2);
     const [usedFiftyFifty, setUsedFiftyFifty] = useState(false);
     const [time, setTime] = useState({});
 
@@ -28,9 +32,11 @@ const Play = () => {
                 setNextQuestion(questions[currentQuestionIndex + 1]);
                 setPreviousQuestion(questions[currentQuestionIndex - 1]);
                 setNumberOfQuestion(questions.length);
+                setPreviousRandomNumbers([]);
             }
         }
         displayQuestions();
+        showOption();
     }, [currentQuestionIndex]);
 
     const handleOptionClick = (e) => {
@@ -48,8 +54,25 @@ const Play = () => {
         }
     }
 
-    const handleButtonClick = () => {
+    const handleNextButtonClick = () => {
         playButtonSound();
+        if (nextQuestion !== undefined) {
+            setCurrentQuestionIndex(currentQuestionIndex + 1);
+        }
+    }
+
+    const handlePreviousButtonClick = () => {
+        playButtonSound();
+        if (previousQuestion !== undefined) {
+            setCurrentQuestionIndex(currentQuestionIndex - 1);
+        }
+    }
+
+    const handleQuitButtonClick = () => {
+        playButtonSound();
+        if (window.confirm('You are sure to quit the game?')) {
+            history.push("/");
+        }
     }
 
     const playButtonSound = () => {
@@ -65,7 +88,7 @@ const Play = () => {
 
         setScore(score + 1);
         setCorrectAnswer(correctAnswer + 1);
-        setcurrentQuestionIndex(currentQuestionIndex + 1);
+        setCurrentQuestionIndex(currentQuestionIndex + 1);
         setNumberOfAnsweredQuestion(numberOfAnsweredQuestion + 1);
     }
 
@@ -78,8 +101,44 @@ const Play = () => {
         });
 
         setWrongAnswers(wrongAnswers + 1);
-        setcurrentQuestionIndex(currentQuestionIndex + 1);
+        setCurrentQuestionIndex(currentQuestionIndex + 1);
         setNumberOfAnsweredQuestion(numberOfAnsweredQuestion + 1);
+    }
+
+    const showOption = () => {
+        const options = Array.from(document.querySelectorAll('.option'));
+        options.forEach(option => {
+            option.style.visibility = 'visible';
+        })
+    }
+
+    const handleHints = () => {
+        if (hints > 0) {
+            const options = Array.from(document.querySelectorAll('.option'));
+            let indexOfAnswer;
+
+            options.forEach((option, index) => {
+                if (option.innerHTML === currentQuestion.answer) {
+                    indexOfAnswer = index;
+                }
+            });
+
+            while (true) {
+                const randomNumber = Math.round(Math.random() * 3);
+                if (randomNumber !== indexOfAnswer && !previousRandomNumbers.includes(randomNumber)) {
+                    options.forEach((option, index) => {
+                        if (index === randomNumber) {
+                            option.style.visibility = 'hidden';
+                            setHints(hints - 1);
+                            setPreviousRandomNumbers(previousRandomNumbers.concat(randomNumber));
+                        }
+                    });
+                    break;
+                }
+                if (previousRandomNumbers.length >= 3)
+                    break;
+            }
+        }
     }
 
     return (
@@ -93,13 +152,13 @@ const Play = () => {
             <div className="question-container">
                 <h2>Quiz Mode</h2>
                 <div className="lifeline-container">
-                    <p>
+                    <p className="help-container">
                         <span className="mdi mdi-set-center mdi-24px lifeline-icon"></span>
-                        <span className="lifeline">2</span>
+                        <span className="lifeline">{fiftyFifty}</span>
                     </p>
-                    <p>
-                        <span className="mdi mdi-lightbulb-on-outline mdi-24px lifeline-icon"></span>
-                        <span className="lifeline">5</span>
+                    <p className="help-container">
+                        <span onClick={handleHints} className="mdi mdi-lightbulb-on-outline mdi-24px lifeline-icon"></span>
+                        <span className="lifeline">{hints}</span>
                     </p>
                 </div>
                 <div className="lifeline-container">
@@ -123,9 +182,9 @@ const Play = () => {
                     <p onClick={handleOptionClick} className="option">{currentQuestion.optionD}</p>
                 </div>
                 <div className="button-container">
-                    <button onClick={handleButtonClick}>Previous</button>
-                    <button onClick={handleButtonClick}>Next</button>
-                    <button onClick={handleButtonClick}>Quit</button>
+                    <button onClick={handlePreviousButtonClick}>Previous</button>
+                    <button onClick={handleNextButtonClick}>Next</button>
+                    <button onClick={handleQuitButtonClick}>Quit</button>
                 </div>
             </div>
         </>
